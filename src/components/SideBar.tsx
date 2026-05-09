9576a063d4cc6798307d8517e34b68ed06b29e34
royaly-dev <clemgc33@gmail.com>
2026-05-09 23:50:11 +0200

    finished the side bar animation !!!!

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,12 +2,21 @@ import { Home, PersonStanding, Settings } from "lucide-react";
 import { useState } from "react";
 
 export default function SideBar() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentSection, setCurrentSection] = useState<
+    "home" | "character" | "settings"
+  >("home");
+  const [startAnimationMilis, setStartAnimationMilis] = useState<number>(0);
+  const [currentAnimationDelay, setCurrentAnimationDelay] = useState<number>(0);
 
   return (
     <div>
       <div
         key={isOpen ? "open" : "closed"}
+        style={{
+          animationDelay:
+            currentAnimationDelay > 0 ? `-${currentAnimationDelay}ms` : "0ms",
+        }}
         className={
           "bg-[rgb(39,41,46)] z-10 h-72 absolute left-7 top-1/2 -translate-y-1/2 w-18.5 rounded-[1.25rem] border-[rgb(66,68,74)] border-solid border " +
           (isOpen ? "animate-sidebar" : "animate-sidebar-reverse")
@@ -17,15 +26,67 @@ export default function SideBar() {
         <Home
           size={32}
           className="p-4 box-content rounded-full text-[rgb(160,170,182)] transition-all duration-75 cursor-pointer hover:bg-[rgb(52,54,60)] hover:text-[rgb(242,246,250)] active:scale-95"
-          onClick={() => setIsOpen(!isOpen)}
+          onClick={() => {
+            setCurrentSection("home");
+            if (isOpen) {
+              if (startAnimationMilis > 0) {
+                const delay = Date.now() - startAnimationMilis;
+                if (delay < 2000) {
+                  const newDelay = 2000 - delay;
+                  setCurrentAnimationDelay(newDelay);
+                  setStartAnimationMilis(Date.now() - newDelay);
+                } else {
+                  setCurrentAnimationDelay(0);
+                  setStartAnimationMilis(Date.now());
+                }
+              }
+              setIsOpen(false);
+            }
+          }}
         />
         <PersonStanding
           size={32}
           className="p-4 box-content rounded-full text-[rgb(160,170,182)] transition-all duration-75 cursor-pointer hover:bg-[rgb(52,54,60)] hover:text-[rgb(242,246,250)] active:scale-95"
+          onClick={() => {
+            setCurrentSection("character");
+            if (!isOpen) {
+              if (startAnimationMilis > 0) {
+                const delay = Date.now() - startAnimationMilis;
+                if (delay < 2000) {
+                  const newDelay = 2000 - delay;
+                  setCurrentAnimationDelay(newDelay);
+                  setStartAnimationMilis(Date.now() - newDelay);
+                } else {
+                  setCurrentAnimationDelay(0);
+                  setStartAnimationMilis(Date.now());
+                }
+              } else {
+                setStartAnimationMilis(Date.now());
+              }
+              setIsOpen(true);
+            }
+          }}
         />
         <Settings
           size={32}
           className="p-4 box-content rounded-full text-[rgb(160,170,182)] transition-all duration-75 cursor-pointer hover:bg-[rgb(52,54,60)] hover:text-[rgb(242,246,250)] active:scale-95"
+          onClick={() => {
+            setCurrentSection("settings");
+            if (!isOpen) {
+              if (startAnimationMilis > 0) {
+                const delay = Date.now() - startAnimationMilis;
+                if (delay < 2000) {
+                  const newDelay = 2000 - delay;
+                  setCurrentAnimationDelay(newDelay);
+                  setStartAnimationMilis(Date.now() - newDelay);
+                } else {
+                  setCurrentAnimationDelay(0);
+                  setStartAnimationMilis(Date.now());
+                }
+              }
+              setIsOpen(true);
+            }
+          }}
         />
       </div>
     </div>
